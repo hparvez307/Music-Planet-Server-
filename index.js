34bb6a87c0e8f8c18c60e204f87e057a33868bc7
hparvez307 <hparvez307@gmail.com>
2023-06-08 16:44:40 +0600

    varify admin

--- index.js
+++ index.js
@@ -128,6 +128,18 @@ async function run() {
 
         })
 
+        app.patch('/makeInstructor/:id', verifyJWT, verifyAdmin, async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updateRole = {
+                $set: { role: 'instructor' }
+            }
+            const result = await usersCollection.updateOne(query, updateRole);
+            res.send(result)
+
+        })
+
 
 
 
